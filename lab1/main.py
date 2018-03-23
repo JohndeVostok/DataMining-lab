dd14f3b0570cfc764c9ef9680572c1ff9fd47ff6
Zixuan <dyxdy@live.com>
2018-03-23 15:26:06 +0800

    fuck main

--- lab1/main.py
+++ lab1/main.py
@@ -27,17 +27,29 @@ nWord = len(data)
 idf = np.zeros((nWord, ), dtype = float)
 tf = np.zeros((nWord, NFILE), dtype = float)
 tf_idf = np.zeros((nWord, NFILE), dtype = float)
+common = np.zeros((nWord, nWord), dtype = int)
+wordList = []
 
-for index, word in enumerate(data):
-	for p in word:
-		tf[index][p[0]] = float(p[1]) / wordCount[index];
-		idf[index] += 1;
+idx = 0
+
+for word in data:
+	wordList.append(word)
+	for p in data[word]:
+		tf[idx][p[0]] = float(p[1]) / wordCount[p[0]];
+		idf[idx] += 1;
+	idx += 1
 
 for index in range(idf.shape[0]):
-	idf[index] = numpy.log(NFILE / (1 + idf[index]))
+	idf[index] = np.log(NFILE / (1 + idf[index]))
 
 for wid in range(tf.shape[0]):
 	for fid in range(tf.shape[1]):
 		tf_idf[wid][fid] = tf[wid][fid] * idf[wid];
 
-print(str(tf[0]))
+for wid1 in range(tf.shape[0]):
+	for wid2 in range(tf.shape[0]):
+		for fid in range(tf.shape[1]):
+			if (tf[wid1][fid] > 0 and tf[wid2][fid] > 0):
+				common[wid1][wid2] += 1
+
+
