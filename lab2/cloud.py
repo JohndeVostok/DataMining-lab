6d5c29e02cd0a9c126662be9d13733ef9a68cc5f
Zixuan <dyxdy@live.com>
2018-04-06 18:05:57 +0800

    wc

--- lab2/cloud.py
+++ lab2/cloud.py
@@ -1,4 +1,5 @@
 import pickle
+from wordcloud import WordCloud
 
 with open("attr.pkl", "rb") as f:
     attrList = pickle.load(f)
@@ -15,3 +16,6 @@ text = ' '.join(wordList)
 
 with open("fullText.txt", "w") as f:
     f.write(text)
+
+wc = WordCloud(background_color = 'white', scale = 1.5).generate(text)
+wc.to_file('wordcloud.jpg')
